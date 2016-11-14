caeb756724328525fffe15799bdb77e1e16b7015
kalyan <kasubram@microsoft.com>
2016-11-14 17:07:22 +0530

    fix the typo

--- src/Agent.Worker/StepsRunner.cs
+++ src/Agent.Worker/StepsRunner.cs
@@ -66,7 +66,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
                     Trace.Info("Skipping step.");
                     step.ExecutionContext.Result = TaskResult.Skipped;
                     
-                    // Complete the step context.
+                    // Skip the step context.
                     step.ExecutionContext.Skip();
 
                     continue;
